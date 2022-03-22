e68668ccc4d616832540c7e67aa04a8bc5516be3
hugosenari <hugosenari@gmail.com>
2022-03-22 00:33:32 -0300

    chore: re-index ../nimpkgs/i/io-usb

--- master/flake.nix
+++ master/flake.nix
@@ -9,8 +9,8 @@
   
   inputs.src-io-usb-master.flake = false;
   inputs.src-io-usb-master.owner = "nimious";
-  inputs.src-io-usb-master.ref   = "refs/heads/master";
-  inputs.src-io-usb-master.repo  = "io-usb";
+  inputs.src-io-usb-master.ref   = "master";
+  inputs.src-io-usb-master.repo  = "io-usb.git";
   inputs.src-io-usb-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
